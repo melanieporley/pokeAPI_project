22876a1834891c096628596dd21618bc5f8c8dbc
[MelaniePorley] <melanie.porley@gmail.com>
2023-09-26 22:54:02 -0300

    new style and functionality added

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => { //Fetch inicial al cargar la página que trae desde la API una lista de pokemons con su nombre y url con información específica
-  const url = "https://pokeapi.co/api/v2/pokemon/";
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      showPokemons(data.results);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-
-})
-
 const container = document.getElementById("container");
 
 
@@ -59,6 +45,7 @@ function pokemonCardReverse(hp, attack, defense, speed) {
 }
 
 function showPokemons(array) {
+  container.innerHTML = '';
   let pokemons = [];
   array.forEach(element => {
 
@@ -75,12 +62,12 @@ function showPokemons(array) {
         const img = data.sprites.front_default;
 
         const pokemonDiv = document.createElement("div");
-        pokemonDiv.classList.add('col');
+        pokemonDiv.classList.add('col', 'd-flex', 'justify-content-center');
 
         pokemonDiv.innerHTML += pokemonCard(img, name);
         pokemons.push(pokemonDiv);
 
-        if (pokemons.length % 5 === 0) {  //Luego de crear la pokemonCard, si la lista pokemons ya tiene 5 tarjetas, crea una nueva fila 
+        if (pokemons.length % 4 === 0) {  //Luego de crear la pokemonCard, si la lista pokemons ya tiene 5 tarjetas, crea una nueva fila 
           divRow = document.createElement("div");
           divRow.classList.add('row');
           for (let item of pokemons) { //bucle que agrega una por una las pokemonCards en pokemons a la fila
@@ -108,3 +95,95 @@ function showPokemons(array) {
 };
 
 
+let nextPage = "";
+let prevPage = "";
+
+function getUrls(next, prev) { 
+  nextPage = next;
+  prevPage = prev;
+   return next, prev;
+}
+const urlInitial = "https://pokeapi.co/api/v2/pokemon";
+
+document.addEventListener('DOMContentLoaded', ()=> { //Fetch inicial al cargar la página que trae desde la API una lista de pokemons con su nombre y url con información específica
+  
+  fetchingData(urlInitial);
+    });
+
+function fetchingData(url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const results = data.results;
+      getUrls(data.next, data.previous);
+      showPokemons(results);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+}
+
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+
+prevBtn.addEventListener('click', ()=> {
+  if(prevPage){
+    fetchingData(prevPage);
+   }
+  });
+
+  nextBtn.addEventListener('click', ()=> {
+   if(nextPage){
+    fetchingData(nextPage);
+   }
+  } );
+
+  const searchInput = document.getElementById('input');
+  const searchBtn = document.getElementById('search');
+  const cleanBtn = document.getElementById('clean');
+
+  searchBtn.addEventListener('click', ()=> {
+    
+    container.innerHTML = ' ';
+
+    const pokemonName = searchInput.value.toLowerCase();
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => response.json())
+    .then(data => {
+  
+      const hp = data.stats[0].base_stat;
+      const attack = data.stats[1].base_stat;
+      const defense = data.stats[2].base_stat;
+      const speed = data.stats[5].base_stat;
+      const name = data.name;
+      const img = data.sprites.front_default;
+
+      const pokemonDiv = document.createElement("div");
+
+      pokemonDiv.innerHTML += pokemonCard(img, name);
+
+      let cardToggle = true;
+
+      pokemonDiv.addEventListener('click', () => {
+        if (cardToggle) {
+          pokemonDiv.innerHTML = ' ';
+          pokemonDiv.innerHTML += pokemonCardReverse(hp, attack, defense, speed);
+          cardToggle = false;
+        } else {
+          pokemonDiv.innerHTML = ' ';
+          pokemonDiv.innerHTML += pokemonCard(img, name);
+          cardToggle = true;
+        }
+
+      });
+      pokemonDiv.classList.add('justify-content-center', 'd-flex');
+      container.appendChild(pokemonDiv);
+    })
+  });
+ 
+  cleanBtn.addEventListener('click', ()=> {
+    container.innerHTML += '';
+    searchInput.value = "";
+    fetchingData(urlInitial);
+  })
